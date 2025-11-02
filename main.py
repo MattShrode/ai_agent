dd970aa936e83c46b2345fbd3bb01b30a1084f87
Matt Shrode <mattshrode@gmail.com>
2025-11-01 23:53:03 -0500

    Get prompt input from command line.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,8 +9,9 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    prompt = sys.argv[1]
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model = 'gemini-2.0-flash-001', contents = prompt
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
@@ -17,4 +19,9 @@ def main():
 
 
 if __name__ == "__main__":
+    try:
+        prompt = sys.argv[1]
+    except IndexError:
+        print("Prompt not provided.")
+        sys.exit(1)
     main()
