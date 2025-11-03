79965271928650436d627719df4e4d5420986ec2
Matt Shrode <mattshrode@gmail.com>
2025-11-02 22:11:03 -0600

    Store user prompt in a list.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,8 +11,13 @@ client = genai.Client(api_key=api_key)
 
 def main():
     prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role = "user", parts = [types.Part(text = prompt)])
+    ]
+
     response = client.models.generate_content(
-    model = 'gemini-2.0-flash-001', contents = prompt
+    model = 'gemini-2.0-flash-001', contents = messages
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
