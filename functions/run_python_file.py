5ad8846328cf71d57b992f4ebd16a4c0423770ce
Matt Shrode <mattshrode@gmail.com>
2025-11-13 23:42:26 -0600

    Work on the subprocess.

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path, args=[]):
     
@@ -15,3 +16,8 @@ def run_python_file(working_directory, file_path, args=[]):
     root, ext = os.path.splitext(file_path)
     if not ext == ".py":
         return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        result = subprocess.run(["python", file_path] + args, cwd=abs_working_dir, capture_output=True, timeout=30, text=True)
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
